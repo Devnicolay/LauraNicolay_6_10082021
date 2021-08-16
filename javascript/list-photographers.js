6ff79dcf7b04a73843658295a86dc0cfa0f81f78
Laura Nicolay <devnicolay@gmail.com>
2021-08-16 18:28:37 +0200

    Rectification resultat promesse

--- javascript/list-photographers.js
+++ javascript/list-photographers.js
@@ -1,10 +1,11 @@
-async function displayPhotographers(result) {
+async function displayPhotographers() {
   try {
     const dataPhotographers = await fetch(
       "./data-photographers/photographers.json"
     );
-    const data = await result.json();
+    const data = await dataPhotographers.json();
     console.log("les photographes sont chargés");
+    return data;
   } catch (exception) {
     debugger;
     console.log("attention une erreur a été rencontrée");
