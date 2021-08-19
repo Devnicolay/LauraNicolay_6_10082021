d4c6695bc858614c903a8508d0c919d36df6bec0
Laura Nicolay <devnicolay@gmail.com>
2021-08-19 16:21:47 +0200

    Réorganisation avec fonction pour afficher photographes de la page d'accueil

--- javascript/list-photographers.js
+++ javascript/list-photographers.js
@@ -1,39 +1,52 @@
-const body = document.querySelector("body");
-const containerPhotographers = document.querySelector(".Photographers");
-window.onload = displayPhotographers();
-async function displayPhotographers() {
+// Collect json data
+async function fetchPhotographers() {
   try {
     const dataPhotographers = await fetch(
       "./data-photographers/photographers.json"
     );
     const data = await dataPhotographers.json();
-    for (var i = 0; i < data.photographers.length; i++) {
-      const portraitPhotographer = data.photographers[i].portrait;
-      const namePhotographer = data.photographers[i].name;
-      const cityPhotographer = data.photographers[i].city;
-      const taglinePhotographer = data.photographers[i].tagline;
-      const pricePhotographer = data.photographers[i].price;
-      const tagsPhotographer = data.photographers[i].tags;
-
-      containerPhotographers.innerHTML +=
-        "<article><a href=#><img src=" +
-        portraitPhotographer +
-        "><h2>" +
-        namePhotographer +
-        "</h2></a><p class=city>" +
-        cityPhotographer +
-        "</p><p class=slogan>" +
-        taglinePhotographer +
-        "</p><p class=prices>" +
-        pricePhotographer +
-        "€/jour</p><div aria-label=tag filter><ul class=tags><a href=#><li><span aria-hidden=true>#</span>" +
-        tagsPhotographer.join(
-          "</li></a><a href=#><li><span aria-hidden=true>#</span>"
-        ) +
-        "</li></a></ul></div></article>";
-    }
     return data;
   } catch (exception) {
     console.log("attention une erreur a été rencontrée");
   }
+}
+
+// Template photographer
+const containerPhotographers = document.querySelector(".Photographers");
+
+async function createTemplate(photographers) {
+  const portraitPhotographer = photographers.portrait;
+  const namePhotographer = photographers.name;
+  const cityPhotographer = photographers.city;
+  const taglinePhotographer = photographers.tagline;
+  const pricePhotographer = photographers.price;
+  const tagsPhotographer = photographers.tags;
+
+  containerPhotographers.innerHTML +=
+    "<article><a href=#><img src=" +
+    portraitPhotographer +
+    "><h2>" +
+    namePhotographer +
+    "</h2></a><p class=city>" +
+    cityPhotographer +
+    "</p><p class=slogan>" +
+    taglinePhotographer +
+    "</p><p class=prices>" +
+    pricePhotographer +
+    "€/jour</p><div aria-label=tag filter><ul class=tags><a href=#><li><span aria-hidden=true>#</span>" +
+    tagsPhotographer.join(
+      "</li></a><a href=#><li><span aria-hidden=true>#</span>"
+    ) +
+    "</li></a></ul></div></article>";
+}
+
+// Apply show all photographer
+window.onload = displayPhotographers();
+async function displayPhotographers() {
+  const photographerData = await fetchPhotographers();
+  const showAll = photographerData.photographers;
+  containerPhotographers.innerHTML = "";
+  showAll.forEach((photographer) => {
+    createTemplate(photographer);
+  });
 }
