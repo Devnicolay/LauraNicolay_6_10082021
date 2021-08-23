2bbc8a165065bde55901bdfe75b7f2ea2b70e567
Laura Nicolay <devnicolay@gmail.com>
2021-08-23 14:41:06 +0200

    Finalisation lien Passer au contenu au scroll de la page d'accueil

--- javascript/scroll-link.js
+++ javascript/scroll-link.js
@@ -1,21 +1,11 @@
-const lastScrollPosition = 0;
-var ticking = false;
 const containerDivScroll = document.querySelector(".scroll-link");
 
-function displayDiv(position_scroll) {
-  // faire quelque chose avec la position du scroll
-  containerDivScroll.innerHTML = "<a href=#ancre-main>Passer au contenu</a>";
-}
+window.addEventListener("scroll", displayDivScroll);
 
-window.addEventListener("scroll", function (e) {
-  lastScrollPosition = window.scrollY;
-
-  if (!ticking) {
-    window.requestAnimationFrame(function () {
-      displayDiv(lastScrollPosition);
-      ticking = false;
-    });
+function displayDivScroll() {
+  if (window.scrollY > 200) {
+    containerDivScroll.style.display = "block";
+    containerDivScroll.innerHTML = "<a href=#ancre-main>Passer au contenu</a>";
+    console.log("marche");
   }
-
-  ticking = true;
-});
+}
