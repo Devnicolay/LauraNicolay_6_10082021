1af3e690783db0c7c04d86a6c33a1b41b0dbf50d
Laura Nicolay <devnicolay@gmail.com>
2021-10-12 08:51:21 +0200

    modification imports et chemin de données

--- javascript/page-index/tags-filters.js
+++ javascript/page-index/tags-filters.js
@@ -1,6 +1,11 @@
+import { ApiFisheye } from "../api-fisheye.js";
+import { Photographer } from "../photographers.js";
+
+const containerPhotographers = document.querySelector(".Photographers");
+
 // Apply filters tags
 const tags = document.querySelectorAll(".tag");
-tags.forEach((tag, index) => {
+tags.forEach((tag) => {
   const tagValue = tag.getAttribute("data");
   tag.addEventListener("click", () => {
     displayFilter(tagValue);
@@ -8,12 +13,10 @@ tags.forEach((tag, index) => {
 });
 
 async function displayFilter(tagValue) {
-  const photographers = await fetchPhotographers();
-  const filteredPhotographers = photographers.photographers.filter(
-    (photographer) => {
-      return photographer.tags.includes(tagValue);
-    }
-  );
+  const photographers = await ApiFisheye.getPhotographers();
+  const filteredPhotographers = photographers.filter((photographer) => {
+    return photographer.tags.includes(tagValue);
+  });
   containerPhotographers.innerHTML = "";
   filteredPhotographers.forEach((photographer) => {
     const photographerConstructor = new Photographer(photographer);
