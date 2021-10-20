57848410fd75dfbfa652b57667791423c6e6ef09
Laura Nicolay <devnicolay@gmail.com>
2021-10-20 10:01:14 +0200

    Restructuration tags-filters.js en classe

--- javascript/page-index/list-photographers.js
+++ javascript/page-index/list-photographers.js
@@ -6,46 +6,25 @@ import { TagsFilter } from "./tags-filters.js";
 // Dom in home page
 const containerPhotographers = document.querySelector(".Photographers");
 
+export class HomePage {
+  /**
+   * Display photographer on home page
+   */
+  static async initHomePage() {
+    let photographers = await ApiFisheye.getPhotographers();
+
+    containerPhotographers.innerHTML = "";
+    photographers.forEach((photographer) => {
+      containerPhotographers.innerHTML += photographer.createTemplateIndex();
+    });
+    /**
+     * Apply filters tags
+     */
+    new TagsFilter(photographers);
+  }
+}
+
 /**
  * loading home page
  */
-window.onload = loadIndex();
-function loadIndex() {
-  displayPhotographer();
-  getUrlFilter();
-}
-
-/**
- * Display photographer on home page
- */
-async function displayPhotographer() {
-  let photographers = await ApiFisheye.getPhotographers();
-  containerPhotographers.innerHTML = "";
-  photographers.forEach((photographer) => {
-    containerPhotographers.innerHTML += photographer.createTemplateIndex();
-  });
-}
-
-/**
- * Apply filters tags
- */
-const tags = document.querySelectorAll(".tag");
-tags.forEach((tag) => {
-  const tagValue = tag.getAttribute("data-filter");
-  tag.addEventListener("click", () => {
-    TagsFilter.displayPhotographersFiltered(tagValue);
-  });
-});
-
-/**
- * When click tag on photographer page, redirect on home page with filtered photographers with tag clicked
- */
-function getUrlFilter() {
-  const ancre = window.location.hash;
-  const deleteHashtag = ancre.substring(1, 13);
-  if (ancre == "") {
-    console.log("none");
-  } else {
-    TagsFilter.displayPhotographersFiltered(deleteHashtag);
-  }
-}
+window.onload = HomePage.initHomePage();
--- javascript/page-index/tags-filters.js
+++ javascript/page-index/tags-filters.js
@@ -1,22 +1,47 @@
-import { ApiFisheye } from "../api-fisheye.js";
-import { Photographer } from "../photographers.js";
-
 export class TagsFilter {
+  constructor(photographers) {
+    this.photographers = photographers;
+    this.initListeners();
+    this.redirectTagFilterHomePage();
+  }
   /**
    *
    * @param {string} tagValue display photographers who have the tag clicked
    */
-  static async displayPhotographersFiltered(tagValue) {
-    const photographers = await ApiFisheye.getPhotographers();
+  displayPhotographersFiltered(tagValue) {
+    const photographers = this.photographers;
     const filteredPhotographers = photographers.filter((photographer) => {
       return photographer.tags.includes(tagValue);
     });
     const containerPhotographers = document.querySelector(".Photographers");
     containerPhotographers.innerHTML = "";
     filteredPhotographers.forEach((photographer) => {
-      const photographerConstructor = new Photographer(photographer);
+      const photographerConstructor = photographer;
       containerPhotographers.innerHTML +=
         photographerConstructor.createTemplateIndex();
+    });
+  }
+
+  /**
+   * When click tag on photographer page, redirect on home page with filtered photographers with tag clicked
+   */
+  redirectTagFilterHomePage() {
+    const ancre = window.location.hash;
+    const deleteHashtag = ancre.substring(1, 13);
+    if (ancre == "") {
+      console.log("none");
+    } else {
+      this.displayPhotographersFiltered(deleteHashtag);
+    }
+  }
+
+  initListeners() {
+    const tags = document.querySelectorAll(".tag");
+    tags.forEach((tag) => {
+      const tagValue = tag.getAttribute("data-filter");
+      tag.addEventListener("click", () => {
+        this.displayPhotographersFiltered(tagValue);
+      });
     });
   }
 }
